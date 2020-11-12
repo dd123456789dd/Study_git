5d4e37fef8a1ace6dd4ac0d66d0500af0f63496a
root <root@itcast.cn>
2020-11-12 15:05:52 +0800

    第三次添加打印输出

--- demo01/src/cn/itheima/Test.java
+++ demo01/src/cn/itheima/Test.java
@@ -4,5 +4,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("the first helloword");
         System.out.println("the second change helloword");
+        System.out.println("the Third change helloword");
     }
 }
