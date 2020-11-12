77c089a5a331cf8e595f9366b92988b74bfeb9e7
root <root@itcast.cn>
2020-11-12 14:34:15 +0800

    添加打印输出信息

--- demo01/src/cn/itheima/Test.java
+++ demo01/src/cn/itheima/Test.java
@@ -3,5 +3,6 @@ package cn.itheima;
 public class Test {
     public static void main(String[] args) {
         System.out.println("the first helloword");
+        System.out.println("the second change helloword");
     }
 }
